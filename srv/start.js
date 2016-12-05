f18caa959bd6c149edd081be7e378d71d460877c
Sebastián Alvarez <seba.alvarez16@gmail.com>
2016-12-05 02:43:43 -0300

    Simplified code

--- srv/start.js
+++ srv/start.js
@@ -2,30 +2,23 @@ import S from 'seneca'
 
 const seneca = S({log: 'test'})
 
-seneca.add({role: 'items', cmd: 'find'}, (msg, done) => {
-	done(null, {id: 0})
-}).
-add({role: 'items', cmd: 'findAll'}, (msg, done) => {
-	done(null, [{id: 0}, {id: 1}])
+seneca.add({role: 'items'}, (msg, done) => {
+	let r = ''
+
+	switch(msg.cmd) {
+		case 'find':
+			r = {id: 0}
+			break
+		case 'findAll':
+			r = [{id: 0}, {id: 1}]
+			break
+		default:
+			r = {}
+	}
+
+	done(null, r)
 }).
 use('mesh', {
 	isbase: true,
 	pin: 'role:items'
 })
-/*
-var Seneca = require('seneca')({log: 'test'})
-
-Seneca.add('format:hex', function (msg, done) {
-
-    // red is the only color supported!
-	var color = 'red' === msg.color ? '#FF0000' : '#FFFFFF'
-
-	done(null, {
-		color: color
-	})
-})
-.use('mesh', {
-	isbase: true,
-	pin: 'format:hex'
-})
-*/
